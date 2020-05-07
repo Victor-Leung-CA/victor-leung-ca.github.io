9b4a83f68944a791bc3db61258283e72c956b793
Victor <victor@victors-mbp.lan>
2020-05-07 03:18:31 -0700

    Responsive timeline and artwork

--- background-animation.js
+++ background-animation.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
           var top= ($win.scrollTop());
           var width= $win.width();
           var percent= top*width*0.0012;
-          var maxdisplacement = 0.75*width;
+          var maxdisplacement = 0.7*width;
       
           if (st > lastScrollTop){
               $plane.css('transform', 'translateX(' +percent+ 'px) scaleX(1)');
@@ -26,8 +26,10 @@ $(document).ready(function(){
          }
       });
 
+
      $button.hover(function(){
           $(this).css('transform', 'scale(1.5,1.5)');
+          $(this).css({"transition":"0.5s"})
      }, function(){
           $(this).css('transform', 'scale(1,1');
      });
@@ -37,3 +39,58 @@ $(document).ready(function(){
      });
 
 });
+
+/* Check the location of each element */
+$('.fadein').each( function(i){
+  
+     var bottom_of_object= $(this).offset().top + $(this).outerHeight();
+     var bottom_of_window = $(window).height();
+     
+     if( bottom_of_object > bottom_of_window){
+       $(this).addClass('hidden');
+     }
+   });
+   
+   
+   $(window).scroll( function(){
+       /* Check the location of each element hidden */
+       $('.hidden').each( function(i){
+         
+           var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+           var bottom_of_window = $(window).scrollTop() + $(window).height();
+         
+           /* If the object is completely visible in the window, fadeIn it */
+           if( bottom_of_window > bottom_of_object ){
+             $(this).animate({'opacity':'1'},700);
+           }
+       });
+   });
+
+   $('#myScrollspy').on('activate.bs.scrollspy', function () {
+     $(this).animate
+   })
+
+   $(document).ready(function(){
+
+     // Add smooth scrolling on all links inside the navbar
+     $(".navbar-nav a").on('click', function(event) {
+       // Make sure this.hash has a value before overriding default behavior
+       if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         event.preventDefault();
+   
+         // Store hash
+         var hash = this.hash;
+   
+         // Using jQuery's animate() method to add smooth page scroll
+         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+         $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 800, function(){
+      
+           // Add hash (#) to URL when done scrolling (default click behavior)
+           window.location.hash = hash;
+         });
+       }  // End if
+     });
+   });
